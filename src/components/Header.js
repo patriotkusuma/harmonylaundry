3cf9564860002a4e0f90a5fcda2b9127ca954906
Patriot Kusuma <patriot.kusumasejati@gmail.com>
2024-02-19 07:31:24 +0700

    add header

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='max-w-screen-lg mx-auto flex items-center py-4 justify-between'>
+        <div className='inline-flex items-center'>
+            <img src='https://harmonylaundry.my.id/assets/img/logo.png' alt='logo Harmony' className='w-[48px]'/>
+            <h1 className='text-blue-900 font-bold text-2xl'>Harmony Laundry</h1>
+        </div>
+
+        
+    </div>
+  )
+}
+
+export default Header
